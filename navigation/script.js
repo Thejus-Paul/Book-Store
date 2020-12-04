23e92599d135a9ae3806c3e1473225a7829ce272
Thejus Paul <thejuspaul@pm.me>
2020-12-04 17:41:31 +0530

    feat(*): More Styling
    feat():
    1. added icons for navigation and genre.
    2. added sample books and its layout.

--- navigation/script.js
+++ navigation/script.js
@@ -1,8 +1,12 @@
 function prevWallpaper() {  
     let carousel = document.querySelector(".carousel");
     carousel.style.background = "url('../images/carousel/1.png')";
+    carousel.style.backgroundSize = "contain";
+    carousel.style.backgroundPosition = "center";
  }
  function nextWallpaper() {
     let carousel = document.querySelector(".carousel");
     carousel.style.background = "url('../images/carousel/2.png')";
+    carousel.style.backgroundSize = "contain";
+    carousel.style.backgroundPosition = "center";
  }
