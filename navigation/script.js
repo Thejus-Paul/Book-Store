33a9d19c8dd6556a58b965e2d370d868bda96358
Thejus Paul <thejuspaul@pm.me>
2020-12-04 12:03:24 +0530

    feat(): Carousel Functionality

---
+++ navigation/script.js
@@ -0,0 +1,8 @@
+function prevWallpaper() {  
+    let carousel = document.querySelector(".carousel");
+    carousel.style.background = "url('../images/carousel/1.png')";
+ }
+ function nextWallpaper() {
+    let carousel = document.querySelector(".carousel");
+    carousel.style.background = "url('../images/carousel/2.png')";
+ }
